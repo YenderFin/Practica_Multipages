7f5cc09cc26c7e239f16639cfe8fdf15bc01dab6
YenderFin <yenderson30503779@gmail.com>
2024-11-01 12:36:35 -0500

    Se agregó astro icons, uso de clases personalizadas y fuentes personalizadas

--- multipages/tailwind.config.mjs
+++ multipages/tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {      fontFamily: {
+			myfont: ['MyFont', ...defaultTheme.fontFamily.sans],
+		  },
+		},
 	},
 	plugins: [],
 }
